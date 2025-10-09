4539518a6c64bbbb36620bc62a9af5f1ede818ea
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-10-09 17:41:35 +0600

    Admin and User Management Seperately

--- index.js
+++ index.js
@@ -1,17 +1,227 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 
-//middleware
-app.use(express.json());
+var admin = require("firebase-admin");
+var serviceAccount = require("./firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// middleware
 app.use(cors());
+app.use(express.json());
+// app.use(requestIp.mw()); // to get IP address
 
-app.get("/", (req, res) =>{
-    res.send("The University Issue Management Server is Started");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dk8ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const userCollection = client.db("propertyDB").collection("users");
+
+
+    //jwt related-----------------------
+
+    // Login API (JWT Token Create)
+    app.post("/login", async (req, res) => {
+      const { email } = req.body;
+      const user = await userCollection.findOne({ email });
+
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      // JWT Token Making (email + role)
+      const token = jwt.sign({ email, role: user.role }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "12h" });
+
+      res.json({ token });
+    });
+
+
+
+
+    //middleware---------------------------
+
+    const verifyJWT = (req, res, next) => {
+      const authHeader = req.headers.authorization;
+
+      if (!authHeader) {
+        return res.status(403).json({ message: "Unauthorized access: No token provided" });
+      }
+
+      const token = authHeader.split(" ")[1]; // "Bearer token" থেকে শুধু টোকেন নিন
+
+      console.log("Received Token:", token);
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          console.error("JWT Verification Error:", err.message);
+          return res.status(403).json({ message: "Invalid token" });
+        }
+
+        req.user = decoded; // টোকেন ডিকোড করে ইউজারের তথ্য সেভ করুন
+        next();
+      });
+    };
+
+
+    // Role-Based Middleware-----------------------------
+
+    // Admin Middleware
+    const verifyAdmin = async (req, res, next) => {
+      const user = await userCollection.findOne({ email: req.user.email });
+
+      if (user?.role !== "Admin") {
+        return res.status(403).json({ message: "Access Denied: Admins only!" });
+      }
+      next();
+    };
+
+    // User Middleware
+    const verifyUser = async (req, res, next) => {
+      const user = await userCollection.findOne({ email: req.user.email });
+
+      if (user?.role !== "User") {
+        return res.status(403).json({ message: "Access Denied: Users only!" });
+      }
+      next();
+    };
+
+    //Admin or User middleware
+    const verifyAdminOrUser = async (req, res, next) => {
+      const user = await userCollection.findOne({ email: req.user.email });
+
+      if (user?.role === "Admin" || user?.role === "User") {
+        return next();
+      }
+
+      return res.status(403).json({ message: "Access Denied: Only Admin or User can perform this action!" });
+    };
+
+
+
+
+
+    //user related api---------------------------
+
+    app.get('/users', verifyJWT, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    //ToDo
+    app.get('/user-email/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await userCollection.find(filter).toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    app.patch('/user-role/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body; // Verified, Rejected, Fraud etc.
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          role: role
+        }
+      };
+
+      const result = await userCollection.updateOne(filter, updatedDoc);
+
+
+      if (role === "Fraud") {
+
+        const user = await userCollection.findOne(filter);
+        if (user?.email) {
+          const propertyFilter = { agent_email: user.email };
+          const deleteResult = await propertyCollection.deleteMany(propertyFilter);
+          console.log(`Deleted ${deleteResult.deletedCount} properties for Fraud agent.`);
+
+          return res.send({
+            modifiedCount: result.modifiedCount,
+            deletedProperties: deleteResult.deletedCount
+          });
+
+        }
+      }
+
+      res.send(result);
+    });
+
+
+    app.delete("/remove-user/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const uid = req.query.uid; // Query থেকে uid নিন
+
+      if (!uid) {
+        return res.status(400).json({ error: "Invalid UID" });
+      }
+
+      try {
+
+        const query = { _id: new ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
+
+        if (result.deletedCount === 0) {
+          return res.status(404).json({ error: "User not found in DB!" });
+        }
+
+        await admin.auth().deleteUser(uid);
+
+        res.json({ success: true, message: "User deleted from MongoDB & Firebase!" });
+
+      } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ error: "Failed to delete user" });
+      }
+    });
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('University Issue Management Server is running here')
 })
 
-app.listen(port, () =>{
-    console.log(`The Server is Working on ${port} port`);
+app.listen(port, () => {
+  console.log(`University Issue Management Server is looked on port ${port}`);
 })
